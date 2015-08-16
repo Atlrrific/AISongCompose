7f8e12e3f23369bcbbbbc63517467b74ff91ffb0
Atl Arredondo <atlsaber@gmail.com>
2015-08-15 18:32:20 -0700

    audioTest to generate sound

---
+++ audioTest.py
@@ -0,0 +1,33 @@
+import math
+import pyaudio
+
+#sudo apt-get install python-pyaudio
+PyAudio = pyaudio.PyAudio
+
+#See http://en.wikipedia.org/wiki/Bit_rate#Audio
+BITRATE = 16000 #number of frames per second/frameset.      
+
+#See http://www.phy.mtu.edu/~suits/notefreqs.html
+FREQUENCY = 161.63 #Hz, waves per second, 261.63=C4-note.
+LENGTH = 5.2232 #seconds to play sound
+
+NUMBEROFFRAMES = int(BITRATE * LENGTH)
+RESTFRAMES = NUMBEROFFRAMES % BITRATE
+WAVEDATA = ''    
+
+for x in xrange(NUMBEROFFRAMES):
+ WAVEDATA = WAVEDATA+chr(int(math.sin(x/((BITRATE/FREQUENCY)/math.pi))*127+128))    
+
+#fill remainder of frameset with silence
+for x in xrange(RESTFRAMES): 
+ WAVEDATA = WAVEDATA+chr(128)
+
+p = PyAudio()
+stream = p.open(format = p.get_format_from_width(1), 
+                channels = 1, 
+                rate = BITRATE, 
+                output = True)
+stream.write(WAVEDATA)
+stream.stop_stream()
+stream.close()
+p.terminate()
