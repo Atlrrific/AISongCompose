04a9ea033e372bf454a674758345f0ab63d4f1b3
Anthony Lowhur <antlowhur@yahoo.com>
2015-08-17 16:31:21 -0700

    Using Atl's edited version, commiting to avoid merge overwritting

--- ai-composer-sound-final.py
+++ ai-composer-sound-final.py
@@ -48,7 +48,7 @@ slash = '/'
 
 # Extract the first 30 elements of the data vector, then convert to list
 
-window_len = 50
+window_len = 500
 
 #window_len = 100
 
@@ -161,7 +161,7 @@ for (sample, target) in ds.getSequenceIterator(0):
                  sequence = sequence + str (int(pred[0])-1600) + ", "
             else:
                 if (int(pred[0])>1000) and (int(pred[0])<2000):
-                    equence = sequence + str (int(pred[0])-1600) + ", "
+                    sequence = sequence + str (int(pred[0])-1600) + ", "
                 else:
                     sequence = sequence + str (int(pred[0])) + ", "
 
